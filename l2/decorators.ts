4ed8df3ba8faf341164c92f8dfc836cdcb1842f4
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2025-12-03 16:16:43 -0300

    lit definitons

---
+++ l2/decorators.ts
@@ -0,0 +1,16 @@
+/// <mls shortName="decorators" project="102027" enhancement="_blank" />
+
+/**
+ * @license
+ * Copyright 2017 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+export * from '/_100000_/customElement.js';
+export * from '/_100000_/property.js';
+export * from '/_100000_/state.js';
+export * from '/_100000_/eventOptions.js';
+export * from '/_100000_/query.js';
+export * from '/_100000_/queryAll.js';
+export * from '/_100000_/queryAsync.js';
+export * from '/_100000_/queryAssignedElements.js';
+export * from '/_100000_/queryAssignedNodes.js';
