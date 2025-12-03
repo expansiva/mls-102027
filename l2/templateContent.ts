a526c38055f5b4310788c91e84f9773c750ad8b7
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2025-12-03 16:18:43 -0300

    lit definitons

---
+++ l2/templateContent.ts
@@ -0,0 +1,28 @@
+/// <mls shortName="templateContent" project="102027" enhancement="_blank" />
+
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+import { noChange } from '/_100000_/litHtml.js';
+import { Directive, PartInfo }  from '/_100000_/directive.js';
+declare class TemplateContentDirective extends Directive {
+    private _previousTemplate?;
+    constructor(partInfo: PartInfo);
+    render(template: HTMLTemplateElement): DocumentFragment | typeof noChange;
+}
+/**
+ * Renders the content of a template element as HTML.
+ *
+ * Note, the template should be developer controlled and not user controlled.
+ * Rendering a user-controlled template with this directive
+ * could lead to cross-site-scripting vulnerabilities.
+ */
+export declare const templateContent: (template: HTMLTemplateElement) => import("/_100000_/directive.js").DirectiveResult<typeof TemplateContentDirective>;
+/**
+ * The type of the class that powers this directive. Necessary for naming the
+ * directive's return type.
+ */
+export type { TemplateContentDirective };
+//# sourceMappingURL=template-content.d.ts.map
