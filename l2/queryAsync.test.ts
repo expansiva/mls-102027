2495921dd0c3deed84e04325ae20dfe16152f101
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-09 10:56:20 -0300

    save: l2/queryAsync.test.ts

--- l2/queryAsync.test.ts
+++ l2/queryAsync.test.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="queryAsync" project="102027" enhancement="_blank" folder="" />
+/// <mls fileReference="_102027_/l2/queryAsync.test.ts" enhancement="_blank" />
 
  import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js';
  export const integrations: ICANIntegration[] = [];
