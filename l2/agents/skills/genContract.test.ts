4412afc7189babb16d89c4a937238fbc4927a4d5
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-05-04 09:32:12 -0300

    Update genContract.test.ts

--- l2/agents/skills/genContract.test.ts
+++ l2/agents/skills/genContract.test.ts
@@ -1,5 +1,2 @@
 /// <mls fileReference="_102027_/l2/agents/skills/genContract.test.ts" enhancement="_blank"/>
 
- import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js';
- export const integrations: ICANIntegration[] = [];
- export const tests: ICANTest[] = [];
