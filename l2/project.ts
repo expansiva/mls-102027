e0b7ad2d4d25e14eb14b61244626d17c047318a8
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-07-26 16:53:47 -0300

    enhancement lit

--- l2/project.ts
+++ l2/project.ts
@@ -1,15 +1,3 @@
 /// <mls fileReference="_102027_/l2/project.ts" enhancement="_blank" />
 
-export const projectConfig = {
-    masterFrontEnd: {
-        build: '',
-        start: '',
-        liveView: '',
-    },
-    masterBackEnd: {
-        build: '',
-        start: '',
-        serverView: ''
-    },
-    modules: []
-}
+export * from '/_102029_/l2/project.js';
